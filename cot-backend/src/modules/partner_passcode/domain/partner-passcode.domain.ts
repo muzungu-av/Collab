0daff2be9bd3e6df60fb718e191d7015ad8168c6
Valera <val12042025@gmail.com>
2025-08-20 19:32:03 +0300

    more complex generatePasscodes method

--- cot-backend/src/modules/partner_passcode/domain/partner-passcode.domain.ts
+++ cot-backend/src/modules/partner_passcode/domain/partner-passcode.domain.ts
@@ -1,7 +1,15 @@
 export class PartnerPasscodeDomain {
   static generatePasscode(): string {
-    // Логика генерации уникального passcode (например, случайная строка)
-    return Math.random().toString(36).substring(2, 12).toUpperCase();
+    const now = new Date();
+    const month = now.getMonth() + 1; // Месяцы в JS начинаются с 0
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const timeString = `${month}-${day}-${hours}-${minutes}-${seconds}`;
+    const timeHash = this.stringToBase36(timeString);
+    const randomPart = Math.random().toString(36).substring(2, 6).toUpperCase();
+    return `${randomPart}${timeHash}`.toUpperCase();
   }
 
   static generatePasscodes(count: number): string[] {
@@ -9,6 +17,18 @@ export class PartnerPasscodeDomain {
     for (let i = 0; i < count; i++) {
       passcodes.push(this.generatePasscode());
     }
+    console.log(JSON.stringify(passcodes));
     return passcodes;
+  }
+
+  // Преобразование в 32-битное целое
+  private static stringToBase36(str: string): string {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash = hash & hash;
+    }
+    return Math.abs(hash).toString(36);
   }
 }
